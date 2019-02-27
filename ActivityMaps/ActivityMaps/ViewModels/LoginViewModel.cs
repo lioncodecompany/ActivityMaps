59045f9fe146f51d6799db44cb73ff08a0965952
lioncodecompany <lioncodecompany@gmail.com>
2019-02-26 22:07:49 -0400

    LoginPage- Kev
    Solo tiene la logica del View y unas cositas en el ViewModel(Back end)

---
+++ ActivityMaps/ActivityMaps/ViewModels/LoginViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ActivityMaps.ViewModels
+{
+	using GalaSoft.MvvmLight.Command;
+	using System.ComponentModel;
+	using Views;
+	using System.Windows.Input;
+	using Xamarin.Forms;
+
+	public class LoginViewModel : BaseViewModel
+	{
+		#region Eventos
+		
+		#endregion
+
+		#region Atributos
+
+		private string email;
+		private bool isEnabled;
+		private bool isRunning;
+		private string password;
+
+		#endregion
+
+		#region Propiedades
+
+		public string Email
+		{
+			get { return this.email; }
+			set { SetValue(ref this.email, value); }
+		}
+
+		public string Password
+		{
+			get { return this.password; }
+			set { SetValue(ref this.password, value); }
+		}
+
+		public bool IsRunning
+		{
+			get { return this.isRunning; }
+			set { SetValue(ref this.isRunning, value); }
+		}
+
+		public bool IsRemembered
+		{
+			get;
+			set;
+		}
+
+		public bool IsEnabled
+		{
+			get { return this.isEnabled; }
+			set { SetValue(ref this.isEnabled, value); }
+		}
+
+		#endregion
+
+		#region Contrusctores
+
+		public LoginViewModel()
+		{
+			this.IsRemembered = true;
+			this.IsEnabled = true;
+
+			this.Email = "k@k.com";
+			this.Password = "1234";
+		}
+
+		#endregion
+		#region Comandos
+
+		public ICommand LoginCommand
+		{
+			get
+			{
+				return new RelayCommand(Login);
+			}
+		}
+
+
+
+		private async void Login()
+		{
+			if (string.IsNullOrEmpty(this.Email))
+			{
+				await Application.Current.MainPage.DisplayAlert(
+					"Error",
+					"You must enter an email.",
+					"Accept");
+				return;
+			}
+
+			if (string.IsNullOrEmpty(this.Password))
+			{
+				await Application.Current.MainPage.DisplayAlert(
+					"Error",
+					"You must enter an Password.",
+					"Accept");
+				return;
+			}
+
+			this.IsRunning = true;
+			this.IsEnabled = false;
+			if (this.Email != "k@k.com" || this.Password != "1234")
+			{
+				this.IsRunning = false;
+				this.IsEnabled = true;
+				await Application.Current.MainPage.DisplayAlert(
+					"Error",
+					"Email or Password is incorrect.",
+					"Accept");
+				this.Password = string.Empty;
+				return;
+			}
+
+			this.IsRunning = false;
+			this.IsEnabled = true;
+
+			this.Email = string.Empty;
+			this.Password = string.Empty;
+
+			//MainViewModel.GetInstance().Lands = new LandsViewModel();
+
+			//await Application.Current.MainPage.Navigation.PushAsync(new LandsPage());
+
+		}
+
+		#endregion
+	}
+}
---
+++ ActivityMaps/ActivityMaps/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityMaps.ViewModels
+{
+	public class MainViewModel
+	{
+		#region ViewModels
+
+		public LoginViewModel Login { get; set; }
+		
+
+		#endregion
+
+		#region Constructors
+
+		public MainViewModel()
+		{
+			instance = this;
+			this.Login = new LoginViewModel();
+		}
+
+
+		#endregion
+
+		#region SignLeton
+		private static MainViewModel instance;
+
+		public static MainViewModel GetInstance()
+		{
+			if(instance == null)
+			{
+				return new MainViewModel();
+			}
+			return instance;
+		}
+		#endregion
+	}
+}
