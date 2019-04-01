2107ab6274548f21ba7124e67a8f36f2ea924afe
lioncodecompany <lioncodecompany@gmail.com>
2019-03-31 20:52:22 -0400

    photojoin - kleon

--- ActivityMaps/ActivityMaps/ViewModels/ActivityJoinViewModel.cs
+++ ActivityMaps/ActivityMaps/ViewModels/ActivityJoinViewModel.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Timers;
+using ActivityMaps.AzureStorage;
 using ActivityMaps.Helpers;
 using ActivityMaps.Models;
+using Xamarin.Forms;
 
 namespace ActivityMaps.ViewModels
 {
 	public class ActivityJoinViewModel : BaseViewModel
 	{
-		
-		
+
+		private Timer TickTimer = null;
 		private Activity selectedActivity;
 		private string userName;
 		private string name;
 		private string categoryName;
 		private string description;
+		private ImageSource image;
 		private bool isRunning;
+
+		[Xamarin.Forms.TypeConverter(typeof(Xamarin.Forms.ImageSourceConverter))]
+		public Xamarin.Forms.ImageSource Image
+		{
+			get { return this.image; }
+			set { SetValue(ref this.image, value); }
+		}
 
 		public bool IsRunning
 		{
@@ -74,14 +86,25 @@ namespace ActivityMaps.ViewModels
 		}
 		public ActivityJoinViewModel(Activity_Child selectedActivity, List<User> userQuery)
 		{
-			
+			TickTimer = new Timer(5000);
+			TickTimer.Elapsed += new ElapsedEventHandler(_Elapsed);
 			this.selectedActivity = selectedActivity;
+			setUserCreated();
 			this.name = selectedActivity.Name;
 			this.categoryName = "Category: "+selectedActivity.CategoryName;
 			this.description = selectedActivity.Description;
-			setUserCreated();
+			
 			//this.UserName = "me " + userQuery[0].Nickname;
 		}
+
+		private void _Elapsed(object sender, ElapsedEventArgs e)
+		{
+			this.Name = this.name;
+			this.CategoryName = this.categoryName;
+			this.Description = this.description;
+			TickTimer.Stop();
+		}
+
 		public ActivityJoinViewModel()
 		{
 
@@ -93,12 +116,18 @@ namespace ActivityMaps.ViewModels
 			var user = await App.MobileService.GetTable<User_Entered>().Where(p => p.Activity_Code_FK2 == selectedActivity.Id && p.IsCreator).ToListAsync();
 			var userCreator = await App.MobileService.GetTable<User_Log>().Where(p => p.Id == user[0].User_Log_Id_FK1).ToListAsync();
 			var userName = await App.MobileService.GetTable<User>().Where(p => p.Id == userCreator[0].User_Id_FK2).ToListAsync();
-			UserName = userName[0].Nickname;
-			this.Name = this.name;
-			this.Description = this.description;
-			this.CategoryName = this.categoryName;
+			var filePath = await App.MobileService.GetTable<File_Path>().Where(p => p.User_Id_FK == userCreator[0].User_Id_FK2).ToListAsync();
+
+			if (filePath.Count > 0)
+			{
+				var imageData = await AzureStorage.AzureStorage.GetFileAsync(ContainerType.Image, filePath[0].Path);
+				Image = ImageSource.FromStream(() => new MemoryStream(imageData));
+			}
+			UserName = "Creator: " + userName[0].Nickname;
+	
 			this.IsRunning = false;
 
 		}
+
 	}
 }
